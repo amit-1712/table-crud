01e6e3b78d3a669a0e42315e5ca9e9529b811213
Amit <amitpalsingh1712@gmail.com>
2022-02-08 12:20:40 +0530

    added

--- chessboard/main.js
+++ chessboard/main.js
@@ -1,122 +1,17 @@
-const chesscontainer = document.getElementById('chess-container');
+var chessBoard = document.getElementById("chess-container");
 
-for(var i = 1; i<9; i++ ){
-    let box1 = document.createElement("div");
-
-    if(i%2==0){
-
-        box1.style.backgroundColor = "white";
-
-    }
-    else{
-        box1.style.backgroundColor = "black";
-    }
-
-    chesscontainer.appendChild(box1);
-    
+var y = window.prompt("enter the number");
+if(y>50){
+    chessBoard.style.width = `${y*2}vw`;
 }
-for(var i = 1; i<9; i++ ){
-    let box2 = document.createElement("div");
 
-    if(i%2==0){
 
-        box2.style.backgroundColor = "black";
+var x=8;
 
+
+for (var i=0; i<y; i++){
+    var row = chessBoard.appendChild(document.createElement("div"));
+    for (var j=0; j<y; j++){
+        row.appendChild(document.createElement("span"));
     }
-    else{
-        box2.style.backgroundColor = "white";
-    }
-
-    chesscontainer.appendChild(box2);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box3 = document.createElement("div");
-
-    if(i%2==0){
-
-        box3.style.backgroundColor = "white";
-
-    }
-    else{
-        box3.style.backgroundColor = "black";
-    }
-
-    chesscontainer.appendChild(box3);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box4 = document.createElement("div");
-
-    if(i%2==0){
-
-        box4.style.backgroundColor = "black";
-
-    }
-    else{
-        box4.style.backgroundColor = "white";
-    }
-
-    chesscontainer.appendChild(box4);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box5 = document.createElement("div");
-
-    if(i%2==0){
-
-        box5.style.backgroundColor = "white";
-
-    }
-    else{
-        box5.style.backgroundColor = "black";
-    }
-
-    chesscontainer.appendChild(box5);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box6 = document.createElement("div");
-
-    if(i%2==0){
-
-        box6.style.backgroundColor = "black";
-
-    }
-    else{
-        box6.style.backgroundColor = "white";
-    }
-
-    chesscontainer.appendChild(box6);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box7 = document.createElement("div");
-
-    if(i%2==0){
-
-        box7.style.backgroundColor = "white";
-
-    }
-    else{
-        box7.style.backgroundColor = "black";
-    }
-
-    chesscontainer.appendChild(box7);
-    
-}
-for(var i = 1; i<9; i++ ){
-    let box8 = document.createElement("div");
-
-    if(i%2==0){
-
-        box8.style.backgroundColor = "black";
-
-    }
-    else{
-        box8.style.backgroundColor = "white";
-    }
-
-    chesscontainer.appendChild(box8);
-    
 }
